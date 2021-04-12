9194e606e6853fe8683c53015eb6c4d43dafbe73
Vinesh Bavikkara <63443210+vineshbavikkara@users.noreply.github.com>
2021-04-12 19:21:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+#  For Deployment
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+@app.route('/predict1',methods=['POST'])
+def predict1():
+    response = [x for x in request.form.values()]
+    res = int(response[0])
+    if int(response[0]) == 1: 
+        return render_template("index1.html",answer="1 is Correct Answer")
+    return render_template("index2.html",a=res,answer=" is Wrong Answer")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
