2dc26dc9e6758917772cca45dfc35e6bf87884a5
Vinesh Bavikkara <63443210+vineshbavikkara@users.noreply.github.com>
2021-04-12 18:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def home():
 def predict1():
     response = [x for x in request.form.values()]
     if int(response[0]) == 1: 
-        return render_template("index.html",predicted1="Correct")
-    return render_template("index.html",predicted1="Wrong")
+        return render_template("index1.html",n="Congratz ",answer="Your Answer is Correct")
+    return render_template("index2.html",n="Oh!! Bad Luck ",answer="Your Answer is Wrong")
 
 if __name__=="__main__":
     app.run(debug=True)
